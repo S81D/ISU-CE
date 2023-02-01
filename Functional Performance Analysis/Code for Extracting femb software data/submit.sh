474d6ea0e56b0d61f1f643ef2bead0a8f0d57e7f
Steven Doran <78985334+S81D@users.noreply.github.com>
2023-02-01 15:23:01 -0600

    Delete Functional Performance Analysis directory

--- Functional Performance Analysis/Code for Extracting femb software data/submit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-b=$1
-
-for line in `cat Timestamps.list`
-do
-	sh AllChannels.sh  $line $b 
-done
-
--- Functional Performance Analysis/Code for Extracting femb software data/submit_gain.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-b=$1
-
-for line in `cat TimestampCT5.list`
-do
-	sh Gain.sh  $line $b 
-done
-
